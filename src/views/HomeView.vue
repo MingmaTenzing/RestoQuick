0460ead4f33bcde241770082d165f017ca623bd8
Mingma Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-10-22 00:49:37 +0800

    added primeicons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
+import Features from '@/components/home/Features.vue'
 import NavBar from '@/components/home/NavBar.vue'
 import TitleSection from '@/components/home/TitleSection.vue'
+import BackgroundGrid from '@/components/ui/BackgroundGrid.vue'
 </script>
 
 <template>
-  <main
-    class="bg-background bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px] dark:bg-[oklch(0.14_0_0)] dark:bg-[linear-gradient(to_right,#ffffff1a_1px,transparent_1px),linear-gradient(to_bottom,#ffffff1a_1px,transparent_1px)]"
-  >
+  <main class="gap-6">
+    <!-- gradient grid background -->
     <div class="lg:w-[1200px] m-auto p-4">
       <NavBar />
-      <TitleSection></TitleSection>
     </div>
+    <BackgroundGrid class="">
+      <div class="lg:w-[1200px] m-auto p-4">
+        <TitleSection></TitleSection>
+      </div>
+    </BackgroundGrid>
+
+    <section class="lg:w-[1200px] m-auto p-4">
+      <!-- start of features section aftert the main image -->
+      <Features />
+    </section>
   </main>
 </template>
