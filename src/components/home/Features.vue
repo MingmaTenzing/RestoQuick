fe04bbd548535826b87a954ece552115ac78dcde
Mingma Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-10-22 00:55:36 +0800

    hero section complege

--- src/components/home/Features.vue
+++ src/components/home/Features.vue
@@ -1,49 +1,57 @@
 <script setup lang="ts"></script>
 
 <template>
-  <section class="gap-6 md:gap-20 flex-wrap md:flex-nowrap flex justify-evenly md:flex-row">
-    <div class="w-[200px] md:w-[240px] space-y-4">
+  <section class="gap-4 flex flex-wrap md:flex-nowrap md:justify-between md:flex-row">
+    <div class="w-[150px] md:w-[240px] space-y-4">
       <div class="w-full h-[0.5px] bg-foreground/50"></div>
       <div class="flex items-center gap-2">
         <div class="px-2 py-2 flex justify-center items-center bg-green-600 rounded-lg">
           <i class="pi pi-users text-green-200 text-lg"></i>
         </div>
-        <p>Smart Roster</p>
+        <p class="font-semibold">Smart Roster</p>
       </div>
-      <p>Easily schedule staff, manage shifts, and track attendance—all in one place.</p>
+      <p class="text-sm font-thin">
+        Easily schedule staff, manage shifts, and track attendance—all in one place.
+      </p>
     </div>
 
-    <div class="w-[200px] md:w-[240px] space-y-4">
+    <div class="w-[150px] md:w-[240px] space-y-4">
       <div class="w-full h-[0.5px] bg-foreground/50"></div>
       <div class="flex items-center gap-2">
         <div class="px-2 py-2 flex justify-center items-center bg-blue-600 rounded-lg">
           <i class="pi pi-box text-blue-200 text-lg"></i>
         </div>
-        <p>Inventory Management</p>
+        <p class="font-semibold">Inventory Management</p>
       </div>
-      <p>Easily schedule staff, manage shifts, and track attendance—all in one place.</p>
+      <p class="text-sm font-thin">
+        Easily schedule staff, manage shifts, and track attendance—all in one place.
+      </p>
     </div>
 
-    <div class="w-[200px] md:w-[240px] space-y-4">
+    <div class="w-[150px] md:w-[240px] space-y-4">
       <div class="w-full h-[0.5px] bg-foreground/50"></div>
       <div class="flex items-center gap-2">
         <div class="px-2 py-2 flex justify-center items-center bg-amber-600 rounded-lg">
           <i class="pi pi-file-check text-amber-200 text-lg"></i>
         </div>
-        <p>Taks Management</p>
+        <p class="font-semibold">Taks Management</p>
       </div>
-      <p>Easily schedule staff, manage shifts, and track attendance—all in one place.</p>
+      <p class="text-sm font-thin">
+        Easily schedule staff, manage shifts, and track attendance—all in one place.
+      </p>
     </div>
 
-    <div class="w-[200px] md:w-[240px] space-y-4">
+    <div class="w-[150px] md:w-[240px] space-y-4">
       <div class="w-full h-[0.5px] bg-foreground/50"></div>
       <div class="flex items-center gap-2">
         <div class="px-2 py-2 flex justify-center items-center bg-violet-600 rounded-lg">
           <i class="pi pi-microchip-ai text-violet-200 text-lg"></i>
         </div>
-        <p>AI Booking System</p>
+        <p class="font-semibold">AI Booking System</p>
       </div>
-      <p>Easily schedule staff, manage shifts, and track attendance—all in one place.</p>
+      <p class="text-sm font-light">
+        Easily schedule staff, manage shifts, and track attendance—all in one place.
+      </p>
     </div>
   </section>
 </template>
