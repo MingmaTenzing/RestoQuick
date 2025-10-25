820f9fc3f65a1470d6e169e1e3272b88dba9c267
Mingma Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-10-25 23:58:16 +0800

    about section complete

--- src/components/ui/About_Section_Accordion.vue
+++ src/components/ui/About_Section_Accordion.vue
@@ -1,25 +1,34 @@
 <script lang="ts" setup>
+import type accordionItems from '@/types/about_accordion'
 import { ref } from 'vue'
 
+const items = defineProps<{ item: accordionItems }>()
 const isFeatureHovered = ref(false)
 </script>
 
 <template>
-  <div
-    class="group/feature"
-    @mouseover="isFeatureHovered = true"
-    @mouseleave="isFeatureHovered = false"
-  >
-    <h3 class="group-hover/feature:text-violet-500">Fully Customize the structure of every page</h3>
-    <Transition>
-      <div v-if="isFeatureHovered">
-        <p>
-          Drag in unstyled HTML elements to build exactly what you want — then turn footers, nav
-          bars, and more into components you can reuse.
-        </p>
+  <section class="border-b cursor-pointer p-4 group/feature">
+    <div class="" @click="isFeatureHovered = !isFeatureHovered">
+      <div class="flex justify-between items-center">
+        <!-- icon -->
+        <div class="flex space-x-2 items-center">
+          <i :class="` ${item.icon} `"></i>
+          <h3 class="group-hover/feature:text-violet-500 text-lg font-medium">{{ item.title }}</h3>
+        </div>
+        <div>
+          <i v-if="!isFeatureHovered" class="pi pi-angle-down"></i>
+          <i v-else class="pi pi-angle-up"></i>
+        </div>
       </div>
-    </Transition>
-  </div>
+      <Transition>
+        <div class="p-2" v-if="isFeatureHovered">
+          <p>
+            {{ item.content }}
+          </p>
+        </div>
+      </Transition>
+    </div>
+  </section>
 </template>
 
 <style>
@@ -35,9 +44,9 @@ const isFeatureHovered = ref(false)
   transform: translateY(100%);
   opacity: 0;
 }
-.v-enter-to,
+/* .v-enter-to,
 .v-leave-from {
   transform: translateY(0);
   opacity: 1;
-}
+} */
 </style>
