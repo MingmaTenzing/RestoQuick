5c70fdabd38233183c9487d67d19352c5d324349
Mingma Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-10-27 10:30:29 +0800

    trusted by section completeg

--- src/components/home_components/Trusted_By.vue
+++ src/components/home_components/Trusted_By.vue
@@ -2,73 +2,101 @@
 <template>
   <div class="my-10 space-y-6">
     <div class="lg:w-1/2">
-      <h2 class="text-4xl md:text-6xl">Trusted by 200,000+ leading organizations</h2>
+      <h2 class="text-lg lg:text-4xl  md:text-6xl">Trusted by 1000+ restaurants across Australia</h2>
     </div>
 
     <section class=" flex space-x-4">
-      <div class=" text-white  relative md:w-[304px] w-[100px] h-[200px] md:h-[450px]">
 
-        <div class=" absolute top-0 left-0 w-full h-full">
+      <!-- nepa cafe -->
+      <div class=" text-white group relative md:w-[304px] overflow-hidden   w-[100px] h-[200px] md:h-[450px]">
 
-          <img  src="../../assets/Vibes Ramen.webp" class="  w-full h-full object-cover "></img>
+        <div class=" absolute top-0 left-0 w-full h-full ">
+
+          <img  src="../../assets/Coffee shop.webp" class="group-hover:scale-110 transition-all ease-in-out  duration-200  w-full h-full object-cover "></img>
         </div>
-        <div class=" absolute top-0 left-0 bg-linear-to-bl from-violet-500 to-fuchsia-500 w-full h-full  opacity-20">
+<div class="absolute top-0 left-0 bg-linear-to-bl from-orange-400 via-pink-500 to-purple-600 w-full h-full opacity-25"></div>
 
-        </div>
-        <div class=" flex justify-between flex-col h-full absolute top-0 left-0 text-2xl ">
-          <div class="flex space-x-4 p-2">
-            <p class=" text-4xl">27%</p>
+        <div class="  justify-between flex-col h-full absolute hidden md:flex top-0 left-0 text-2xl ">
+          <div class="flex space-x-4 p-4">
+            <p class=" text-lg lg:text-4xl ">27%</p>
             <p class=" text-sm">Traffic increase one week post launch</p>
           </div>
-          <div class=" p-2 ">
- <p class="text-4xl">
-         Vibes Ramen
+          <div class=" p-4 ">
+ <p class="text-lg lg:text-4xl ">
+         Nepa Cafe
  </p>
           </div>
         </div>
       </div>
-      <div class=" text-white  relative md:w-[304px] w-[100px] h-[200px] md:h-[450px]">
 
-        <div class=" absolute top-0 left-0 w-full h-full">
 
-          <img  src="../../assets//Bella Vista Italian.webp" class="  w-full h-full object-cover "></img>
+      <!-- korean -->
+      <div class=" text-white group relative md:w-[304px] overflow-hidden   w-[100px] h-[200px] md:h-[450px]">
+
+        <div class=" absolute top-0 left-0 w-full h-full ">
+
+          <img  src="../../assets/korean bbq.webp" class="group-hover:scale-110 transition-all ease-in-out  duration-200  w-full h-full object-cover "></img>
         </div>
-        <div class=" absolute top-0 left-0 bg-linear-to-bl from-violet-500 to-fuchsia-500 w-full h-full  opacity-20">
+<div class="absolute top-0 left-0 bg-linear-to-bl from-cyan-400 via-blue-500 to-indigo-600 w-full h-full opacity-25"></div>
 
-        </div>
-        <div class=" flex justify-between flex-col h-full absolute top-0 left-0 text-2xl ">
-          <div class="flex space-x-4 p-2">
-            <p class=" text-4xl">27%</p>
-            <p class=" text-sm">Traffic increase one week post launch</p>
+        <div class="  justify-between flex-col h-full absolute hidden md:flex top-0 left-0 text-2xl ">
+          <div class="flex space-x-4 p-4">
+            <p class=" text-lg lg:text-4xl ">30%</p>
+            <p class=" text-sm">Cost Reduction on Management</p>
           </div>
-          <div class=" p-2 ">
- <p>
-         Bella Vista
+          <div class=" p-4 ">
+ <p class="text-lg lg:text-4xl ">
+         Korean Hamida
  </p>
           </div>
         </div>
       </div>
-      <div class=" text-white  relative md:w-[304px] w-[100px] h-[200px] md:h-[450px]">
 
-        <div class=" absolute top-0 left-0 w-full h-full">
+     <!-- italian restauarnt
+       -->
+      <div class=" text-white group relative md:w-[304px] overflow-hidden   w-[100px] h-[200px] md:h-[450px]">
 
-          <img  src="../../assets/Urban Eats.webp" class="  w-full h-full object-cover "></img>
+        <div class=" absolute top-0 left-0 w-full h-full ">
+
+          <img  src="../../assets/italian restaurant.webp" class="group-hover:scale-110 transition-all ease-in-out  duration-200  w-full h-full object-cover "></img>
         </div>
-        <div class=" absolute top-0 left-0 bg-linear-to-bl from-violet-500 to-fuchsia-500 w-full h-full  opacity-20">
+<div class="absolute inset-0 bg-linear-to-bl from-red-500 via-orange-500 to-yellow-500 opacity-25"></div>
 
-        </div>
-        <div class=" flex justify-between flex-col h-full absolute top-0 left-0 text-2xl ">
-          <div class="flex space-x-4 p-2">
-            <p class=" text-4xl">27%</p>
-            <p class=" text-sm">Traffic increase one week post launch</p>
+        <div class="  justify-between flex-col h-full absolute hidden md:flex top-0 left-0 text-2xl ">
+          <div class="flex space-x-4 p-4">
+            <p class=" text-lg lg:text-4xl ">50%</p>
+            <p class=" text-sm">Less Paper Work Overload</p>
           </div>
-          <div class=" p-2 ">
- <p>
-         Bella Vista
+          <div class=" p-4 ">
+ <p class="text-lg lg:text-4xl ">
+        ByItalian
  </p>
           </div>
         </div>
       </div>
+
+      <!-- ramen restaurant -->
+      <div class=" text-white group relative md:w-[304px] overflow-hidden   w-[100px] h-[200px] md:h-[450px]">
+
+        <div class=" absolute top-0 left-0 w-full h-full ">
+
+          <img  src="../../assets/Ramen restaurant.webp" class="group-hover:scale-110 transition-all ease-in-out  duration-200  w-full h-full object-cover "></img>
+        </div>
+<div class="absolute inset-0 bg-linear-to-bl from-pink-200 via-rose-300 to-red-400 opacity-30"></div>
+
+        <div class="  justify-between flex-col h-full absolute hidden md:flex top-0 left-0 text-2xl ">
+          <div class="flex space-x-4 p-4">
+            <p class=" text-lg lg:text-4xl ">40%</p>
+            <p class=" text-sm">More Bookings Due to AI Receiptionist</p>
+          </div>
+          <div class=" p-4 ">
+ <p class="text-lg lg:text-4xl ">
+        Ramenasigas
+ </p>
+          </div>
+        </div>
+      </div>
+
     </section>
   </div>
 </template>
